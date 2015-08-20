482b576877a726653ad39593274c4a43cff7e94d
Sandesh Devaraju <sdevaraju@appnexus.com>
2015-08-20 20:58:06 +0000

    fix

--- limbo/plugins/genesis.py
+++ limbo/plugins/genesis.py
@@ -1,26 +1,59 @@
 """!genesis returns a random sega genesis screenshot"""
 
-from bs4 import BeautifulSoup
 import re
 import requests
-from random import shuffle, randint
+import hashlib
+from random import choice
+from conf import flickr_client_key, flickr_client_secret
+
+def sign(params):
+    md5 = hashlib.md5()
+    md5.update(flickr_client_secret)
+    for key in sorted(params):
+        md5.update(key)
+        md5.update(str(params[key]))
+    return md5.hexdigest()
+
 
 def genesis():
-    # http://ascii.textfiles.com/archives/4365
-    page = randint(1, 8)
-    r = requests.get("https://secure.flickr.com/photos/textfiles/sets/72157646180733361/page%s/" % page)
-    soup = BeautifulSoup(r.text)
-    images = soup.findAll("img", attrs={"data-defer-src": True})
-    images = [i.attrs["data-defer-src"] for i in images]
+    api_endpoint = "https://api.flickr.com/services/rest"
 
-    shuffle(images)
+    params = {
+        "method": "flickr.photosets.getPhotos",
+        "api_key": flickr_client_key,
+        "photoset_id": 72157646180733361,
+        "format": "json",
+        "nojsoncallback": 1
+    }
+    params["api_sig"] = sign(params)
+    r = requests.get(api_endpoint,
+                     params=params,
+                     verify=False)
+    photo = choice(r.json()["photoset"]["photo"])
 
-    return images[0] if images else ""
+    params = {
+        "method": "flickr.photos.getSizes",
+        "api_key": flickr_client_key,
+        "photo_id": photo["id"],
+        "format": "json",
+        "nojsoncallback": 1
+    }
+    params["api_sig"] = sign(params)
+    r = requests.get(api_endpoint,
+                     params=params,
+                     verify=False)
+
+    for size in r.json()["sizes"]["size"]:
+        if size["label"] == "Medium":
+            return photo["title"] + "\n" + size["source"]
 
 def on_message(msg, server):
     text = msg.get("text", "")
     match = re.findall(r"!genesis", text)
     if not match:
         return
-
     return genesis()
+
+
+if __name__ == "__main__":
+    print genesis()
