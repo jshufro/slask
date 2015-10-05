fa2fa999e3c155a0ffc861a79f7dcd9cfa3591b5
Mark Ha <mha@appnexus.com>
2015-10-05 15:17:22 -0400

    reformatting budgetage

--- limbo/plugins/budgetage_script.py
+++ limbo/plugins/budgetage_script.py
@@ -9,7 +9,7 @@ import conf
 
 username = conf.appnexus_user
 password = conf.appnexus_pass
-DCS = ('nym1', 'nym2', 'lax1', 'ams1', 'sin1', 'fra1')
+# DCS = ('nym1', 'nym2', 'lax1', 'ams1', 'sin1', 'fra1')
 URL_BIDDER = 'https://metrics.adnxs.net/render?from=-30minutes&until=now&target=clusters.prod.bidderc.*.budget.age_seconds&format=json'
 URL_IMPBUS = 'https://metrics.adnxs.net/render?from=-30minutes&until=now&target=clusters.prod.impbus.*.budget.age_seconds&format=json'
 
@@ -32,20 +32,29 @@ def fetch_metrics(url):
     for data in ret:
         dc = data['target'].split('.')[3]
         metrics[dc] = data['datapoints']
-    return  metrics
+    return metrics
+
+def age_to_string(dc_age):
+    return '\n'.join(['%4s: %5d seconds  %s' % (dc, age, '.'* int(age/600)) for dc, age in dc_age.iteritems()])
 
 def on_message(msg, server):
     """!budget: show budget age"""
     text = msg.get("text", "")
-    reg = re.compile('^!budget(\s(\w+))?', re.IGNORECASE)
+    reg = re.compile('!budget', re.IGNORECASE)
     match = reg.match(text)
     if not match:
         return
 
-    url = URL_BIDDER
-    if match.group(2):
-        if match.group(2).lower() == 'impbus':
-            url = URL_IMPBUS
-    metrics = fetch_metrics(url)
-    dc_age = find_latest_age(metrics)
-    return '\n'.join(['%4s: %5d seconds  %s' % (dc, age, '.'* int(age/600)) for dc, age in dc_age.iteritems()])
+    return '\n```BIDDER:\n%s \n\nIMPBUS:\n%s \n```' % (
+           age_to_string(find_latest_age(fetch_metrics(URL_BIDDER))),
+           age_to_string(find_latest_age(fetch_metrics(URL_IMPBUS)))
+    )
+
+
+bidder_age = fetch_metrics(URL_BIDDER)
+impbus_age = fetch_metrics(URL_IMPBUS)
+
+print '\n```BIDDER:\n%s \nIMPBUS:\n%s \n```' % (
+       age_to_string(find_latest_age(fetch_metrics(URL_BIDDER))),
+       age_to_string(find_latest_age(fetch_metrics(URL_IMPBUS)))
+       )
