cb42834ad4a48c7e609b066c8a70b14ab9fd11ce
gsivan <gsivan@appnexus.com>
2015-11-24 16:10:33 -0500

    summarizer first shot

--- limbo/plugins/summarizer.py
+++ limbo/plugins/summarizer.py
@@ -2,8 +2,68 @@ import redis
 import json
 import requests
 import time
+from nltk.tokenize import sent_tokenize,word_tokenize
+from nltk.corpus import stopwords
+from collections import defaultdict
+from string import punctuation
+from heapq import nlargest
 
 from limbo import conf
+
+
+class FrequencySummarizer:
+  def __init__(self, min_cut=0.1, max_cut=0.9):
+    """
+     Initilize the text summarizer.
+     Words that have a frequency term lower than min_cut
+     or higer than max_cut will be ignored.
+    """
+    self._min_cut = min_cut
+    self._max_cut = max_cut
+    self._stopwords = set(stopwords.words('english') + list(punctuation))
+
+  def _compute_frequencies(self, word_sent):
+    """
+      Compute the frequency of each of word.
+      Input:
+       word_sent, a list of sentences already tokenized.
+      Output:
+       freq, a dictionary where freq[w] is the frequency of w.
+    """
+    freq = defaultdict(int)
+    for s in word_sent:
+      for word in s:
+        if word not in self._stopwords:
+          freq[word] += 1
+    # frequencies normalization and fitering
+    m = float(max(freq.values()))
+    for w in freq.keys():
+      freq[w] = freq[w]/m
+      if freq[w] >= self._max_cut or freq[w] <= self._min_cut:
+        del freq[w]
+    return freq
+
+  def summarize(self, text, n):
+    """
+      Return a list of n sentences
+      which represent the summary of text.
+    """
+    sents = sent_tokenize(text)
+    assert n <= len(sents)
+    word_sent = [word_tokenize(s.lower()) for s in sents]
+    self._freq = self._compute_frequencies(word_sent)
+    ranking = defaultdict(int)
+    for i,sent in enumerate(word_sent):
+      for w in sent:
+        if w in self._freq:
+          ranking[i] += self._freq[w]
+    sents_idx = self._rank(ranking, n)
+    return [sents[j] for j in sents_idx]
+
+  def _rank(self, ranking, n):
+    """ return the first n sentences with highest ranking """
+    return nlargest(n, ranking, key=ranking.get)
+
 
 R = redis.StrictRedis(host=conf.redis_host, port=conf.redis_port, db=conf.redis_db)
 
@@ -15,19 +75,24 @@ def get_username(user_id):
     r = requests.get(users_url)
     return r.json().get('user').get('name')
 
-# key-value functions
+def history():
+    resp = ""
+    for k in sorted(R.keys(PREFIX + '*')):
+        for m in R.lrange(k, 0, -1):
+            val = json.loads(m)
+            t = time.strftime("%a, %d %b %Y %H:%M:%S",
+                              time.localtime(int(float(val['ts']))))
+            resp += str(t) + "   " + val['user'] + ": " + val['text'] + "\n"
+    return resp
 
 def on_message(msg, server):
     text = msg.get("text", "")
     if text == "!history":
-        resp = ""
-        for k in sorted(R.keys(PREFIX + '*')):
-            for m in R.lrange(k, 0, -1):
-                val = json.loads(m)
-                t = time.strftime("%a, %d %b %Y %H:%M:%S",
-                                  time.localtime(int(float(val['ts']))))
-                resp += str(t) + val['user'] + ": " + val['text'] + "\n"
-        return resp
+        return history()
+
+    if text == "!summarize":
+        fs = FrequencySummarizer()
+        return fs.summarize(history(), 10)
 
     v = dict()
     v['text'] = text
