ba89f0fccee2e5f1b2201a219eca0036fb01effb
Mark Ha <mha@appnexus.com>
2015-11-24 18:19:13 -0500

    smartass

---
+++ limbo/plugins/smartass.py
@@ -0,0 +1,39 @@
+import re
+import random
+import json
+
+P = .01
+
+replacements = ["mymom", "gooby", "pls", "dolan", "appnexus", "opt"]
+
+def random_response(text, user):
+    words = text.split(" ")
+    word = random.choice(words)
+
+    responses = []
+    responses.append("Very wow")
+    responses.append("Yeezy taught me")
+    responses.append("You who else is {}? #mymom".format(word))
+    responses.append("I agree with {}".format(user))
+    responses.append("Sounds good to me, {}".format(user))
+    responses.append("I'm proud of you {}".format(user))
+    responses.append("That sounds wrong, {}".format(user))
+    responses.append("{}, do you even know what you're talking about?".format(user))
+    responses.append("{}... just no".format(user))
+    for s in replacements:
+        responses.append("or... " + text.replace(word, s))
+
+    return random.choice(responses)
+
+def on_message(msg, server):
+    text = msg.get("text", "")
+    if text.startsWith("!"):
+        return False
+
+    if random.random() < 0.5:
+        response = json.loads(server.slack.api_call("users.info", user=msg["user"]))
+        user = response["user"]["profile"]["real_name"]
+        return random_response(text, user)
+    else:
+        return False
+
