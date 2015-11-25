f297bbb1433dac4b888a57453c760060052840f5
Mark Ha <mha@appnexus.com>
2015-11-25 12:21:45 -0500

    maybe fake ok

--- limbo/plugins/nagios.py
+++ limbo/plugins/nagios.py
@@ -1,5 +1,7 @@
 """!99 problems : all etl opt nagios services that are not OK
-!downtime <service> <duration(minutes)> <host> <comment>: set nagios downtime"""
+!downtime <service> <duration(minutes)> <host> <comment>: set nagios downtime
+!fake_ok <service> <host>: Fake OK result
+"""
 
 import re
 import requests
@@ -8,6 +10,23 @@ from requests.auth import HTTPBasicAuth
 from limbo import conf
 nagios_user = conf.nagios_user
 nagios_pass = conf.nagios_pass
+
+BASE_URL = "https://multimonitor.nym2.adnxs.net/check_mk/view.py?_do_confirm=yes&_transid=-1&_do_actions=yes&actions=yes&filled_in=actions&view_name=service&service={service}&host={host}&output_format=json"
+FAKE_OK = "&_fake_0=OK"
+DOWNTIME = "&_down_from_now=From+now+for&_down_minutes={dur}&_down_comment={comment}"
+
+def make_request(url):
+    response = requests.get(
+        url,
+        auth=HTTPBasicAuth(nagios_user, nagios_pass),
+        verify=False)
+    if response.status_code != 200:
+        return "Error"
+    # For some reason this URL does not return nice json...
+    if response.content.startswith("MESSAGE: Successfully sent 1 commands."):
+        return "Success"
+    else:
+        return "Problem - look at check_mk"
 
 def set_downtime(text):
     match = re.match(r"^!downtime\s+(?P<service>[\w-]+)\s+(?P<dur>\d+)\s*(?P<host>[\w\-\.]+)\s*(?P<comment>.*)\s*$", text, re.IGNORECASE)
@@ -24,22 +43,26 @@ def set_downtime(text):
         host = "{}.bm-etl-optimization.prod.lax1".format(host)
     except ValueError:
         pass
-    url = \
-    "https://multimonitor.nym2.adnxs.net/check_mk/view.py?_do_confirm=yes&_transid=-1&_do_actions=yes&actions=yes&host={host}&view_name=service&site=prod-lax1&service={service}&_cusnot_comment=TEST&_ack_comment=&_comment=&_down_from_date=2014-03-30&_down_from_time=04%3A20&_down_to_date=2014-03-30&_down_to_time=06%3A20&_down_from_now=From+now+for&_down_minutes={dur}&_down_duration=02%3A00&_down_comment={comment}&output_format=json"
+    url = BASE_URL + DOWNTIME
     url = url.format(host=host, service=service, dur=duration, comment=comment)
-    response = requests.get(
-        url,
-        auth=HTTPBasicAuth(nagios_user, nagios_pass),
-        verify=False)
-    if response.status_code != 200:
-        return "Error"
-    # For some reason this URL does not return nice json...
-    status = response.content.split('\n')[0]
-    if re.compile(".*successfully.*", re.IGNORECASE).match(status):
-        return "Downtime set"
-    else:
-        return "Problem - look at check_mk"
+    return make_request(url)
 
+def fake_ok(text):
+    match = re.match(r"^!fake_ok\s+(?P<service>[\w-]+)\s+(?P<host>[\w\-\.]+)\s*$", text, re.IGNORECASE)
+    if not match:
+        return False
+    service = match.group('service')
+    host = match.group('host')
+    if not service or not host:
+        return "All fields required: service, host"
+    try:
+        int(host)
+        host = "{}.bm-etl-optimization.prod.lax1".format(host)
+    except ValueError:
+        pass
+    url = BASE_URL + FAKE_OK
+    url = url.format(host=host, service=service)
+    return make_request(url)
 
 def opt_status(text):
     match = re.match(r"!99 problems\s*$", text)
@@ -68,4 +91,4 @@ def opt_status(text):
 
 def on_message(msg, server):
     text = msg.get("text", "")
-    return set_downtime(text) or opt_status(text)
+    return set_downtime(text) or opt_status(text) or fake_ok(text)
