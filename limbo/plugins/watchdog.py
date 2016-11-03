a229ec0cbba3b1b1069c625caaeb2423936de7c1
Mark Ha <mha@appnexus.com>
2016-11-03 13:18:16 -0400

    oprhans cmd

--- limbo/plugins/watchdog.py
+++ limbo/plugins/watchdog.py
@@ -44,6 +44,49 @@ def all_watchdog_status(body):
     return "```%s```" % output
 
 
+def watchdog_orphans(body):
+    reg = re.compile('!(doge)?([\s|_])?orphans', re.IGNORECASE)
+    match = reg.match(body)
+    if not match:
+        return False
+    url = "https://multimonitor.nym2.adnxs.net/check_mk/view.py?service=etl-optimization-watchdog&opthostgroup=&host=&view_name=servicedesc&st0=on&st1=on&st2=on&st3=on&stp=on&output_format=python"
+    response = requests.get(url, auth=HTTPBasicAuth(nagios_user, nagios_pass), verify=False)
+    data = eval(response.text)
+    output = ""
+
+
+    max_version = ""
+    version_host_list_map = {}
+    for i in data:
+        # status = i[0]
+        host = i[1]
+        message = i[2]
+        match = re.match(r'(.*)version:(?P<version>.*)]:(.*)', message, re.IGNORECASE)
+        if not match:
+            continue
+
+        version = match.group('version')
+        if not version_host_list_map[version]:
+            version_host_list_map[version] = []
+        version_host_list_map[version].append(host)
+
+        if version > max_version:
+            max_version = version
+
+    output += "Latest version:{}\n".format(max_version)
+
+    if len(version_host_list_map) == 1:
+        output += "No orphans!"
+    else:
+        output += "Orphans:\n"
+
+    for version, host_list in version_host_list_map:
+        if version != max_version:
+            host_list_str = "\n".join(host_list)
+            output += "\nVersion: {v}\nHosts:\n{h}".format(v=version, h=host_list_str)
+    return "```%s```" % output
+
+
 def watchdog_log(body):
     reg = re.compile('!watchdog log (.*)', re.IGNORECASE)
     match = reg.match(body)
@@ -62,4 +105,4 @@ def watchdog_log(body):
 
 def on_message(msg, server):
     text = msg.get("text", "")
-    return watchdog_status(text) or all_watchdog_status(text) or watchdog_log(text)
+    return watchdog_status(text) or all_watchdog_status(text) or watchdog_orphans(text) or watchdog_log(text)
